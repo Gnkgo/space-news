c9bd4e2575cd3360edf35511b045a9014621312b
gnkgo <joanna.brodbeck@hotmail.ch>
2023-11-23 08:18:57 +0100

    Start Moon

--- src/client/ts/home.ts
+++ src/client/ts/home.ts
@@ -13,14 +13,14 @@ const sun = document.getElementById('sun');
 
 
 
-function showPlanetInformation(planet: string){
+function showPlanetInformation(planet: string) {
     const body = document.body;
     body.innerHTML = '';
     const head = document.head;
 
     switch (planet) {
         case 'mars':
-            // Add link to mars.css
+
             const marsStylesheet = document.createElement('link');
             marsStylesheet.rel = 'stylesheet';
             marsStylesheet.type = 'text/css';
@@ -32,10 +32,8 @@ function showPlanetInformation(planet: string){
             const script = document.createElement('script');
             script.src = "/src/client/ts/mars.ts";
 
-
             body.appendChild(main);
             body.appendChild(script);
-
 
             break;
         case 'nea':
@@ -47,14 +45,27 @@ function showPlanetInformation(planet: string){
                 .catch(error => console.error('Error loading nea.html:', error));
             break;
         case 'moon':
-            body.innerHTML = '<h1>moon</h1>'
-            body.style.backgroundImage = 'none';
+            // Add link to mars.css
+            const moonStyleSheet = document.createElement('link');
+            moonStyleSheet.rel = 'stylesheet';
+            moonStyleSheet.type = 'text/css';
+            moonStyleSheet.href = '/src/client/css/moon.css';
+            head.appendChild(moonStyleSheet);
+
+            const mainMoon = document.createElement('main'); 
+            mainMoon.id = 'main';
+            const scriptMoon = document.createElement('script');
+            scriptMoon.src = "/src/client/ts/moon.ts";
+
+            body.appendChild(mainMoon);
+            body.appendChild(scriptMoon);
             break;
         default:
             break;
     }
 }
 
-function showSunEasterEgg(){
+
+function showSunEasterEgg() {
 
 }
