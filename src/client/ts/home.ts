db6775129089fa351cd63a095fac0f8289346dff
Nick <ntroxler@ethz.ch>
2023-11-23 12:26:28 +0100

    changed method for displaying planet infos

--- src/client/ts/home.ts
+++ src/client/ts/home.ts
@@ -1,3 +1,5 @@
+import { NONAME } from "dns";
+
 //Add click-eventlistener to planets
 const mars = document.getElementById('mars');
 mars?.addEventListener('click', () => showPlanetInformation('mars'));
@@ -14,74 +16,30 @@ const sun = document.getElementById('sun');
 
 
 function showPlanetInformation(planet: string) {
-    const body = document.body;
-    body.innerHTML = '';
-    const head = document.head;
-
-    const baseStyleSheet = document.createElement('link');
-    baseStyleSheet.rel = 'stylesheet';
-    baseStyleSheet.type = 'text/css';
-    baseStyleSheet.href = '/src/client/css/base.css';
-
-    const componentStyleSheet = document.createElement('link');
-    componentStyleSheet.rel = 'stylesheet';
-    componentStyleSheet.type = 'text/css';
-    componentStyleSheet.href = '/src/client/css/component.css';
-    head.appendChild(componentStyleSheet);
+        const homeContainer = document.getElementById("home-container");
+        if(homeContainer){
+            homeContainer.style.display = 'none';
+        }
 
     switch (planet) {
         case 'mars':
-            const marsStylesheet = document.createElement('link');
-            marsStylesheet.rel = 'stylesheet';
-            marsStylesheet.type = 'text/css';
-            marsStylesheet.href = '/src/client/css/mars.css';
-
-            head.appendChild(baseStyleSheet);
-
-            head.appendChild(componentStyleSheet);
-
-            head.appendChild(marsStylesheet);
-
-    
-            const main = document.createElement('main');
-            main.id = 'main';
-    
-            const script = document.createElement('script');
-            script.type = 'module';
-            script.src = '/src/client/ts/mars.ts';
-    
-            body.appendChild(main);
-            body.appendChild(script);
+            const marsContainer = document.getElementById('mars-container');
+            if(marsContainer){
+                marsContainer.style.display = 'grid';
+            }
             break;
     
         case 'nea':
-            fetch('/nea.html')
-                .then(response => response.text())
-                .then(html => {
-                    body.innerHTML = html;
-                })
-                .catch(error => console.error('Error loading nea.html:', error));
+            const neaContainer = document.getElementById('nea-container');
+            if(neaContainer){
+                neaContainer.style.display = 'grid';
+            }
             break;
         case 'moon':
-            
-        
-            head.appendChild(baseStyleSheet);
-
-            head.appendChild(componentStyleSheet);
-
-            const moonStyleSheet = document.createElement('link');
-            moonStyleSheet.rel = 'stylesheet';
-            moonStyleSheet.type = 'text/css';
-            moonStyleSheet.href = '/src/client/css/moon.css';
-            head.appendChild(moonStyleSheet);
-
-            const mainMoon = document.createElement('main'); 
-            mainMoon.id = 'main';
-            const scriptMoon = document.createElement('script');
-            scriptMoon.src = "/src/client/ts/moon.ts";
-
-            body.appendChild(mainMoon);
-            body.appendChild(scriptMoon);
+            const moonContainer = document.getElementById('moon-container');
+            if(moonContainer){
+                moonContainer.style.display = 'grid';
+            }
             break;
         default:
             break;
