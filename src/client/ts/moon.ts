078f676140f8550525f95e15edc7ecb8a9e1530c
Nick <ntroxler@ethz.ch>
2023-11-24 14:56:23 +0100

    adjust display of moon container

--- src/client/ts/moon.ts
+++ src/client/ts/moon.ts
@@ -1,22 +1,22 @@
+const moonContainer = document.getElementById('moon-container');
+
 function createFooterMoon() {
-    const body = document.body;
     const footer = document.createElement("footer");  
     footer.textContent = "© 2023 by DeValdi - Gnkgo - Nick20500";
-    body?.appendChild(footer);
+    moonContainer?.appendChild(footer);
   }
 
 function initMoon() {
     createFooterMoon();
 
-    const body = document.body;
     const h1 = document.createElement("h1");
     h1.textContent = "Hello I am a Moon`! :D"
 
     const img = document.createElement("img");
     img.src = "/src/client/img/moon.png"
     img.id = "moon";
-    body?.appendChild(img);
-    body?.appendChild(h1);
+    moonContainer?.appendChild(img);
+    moonContainer?.appendChild(h1);
 }
 
 
