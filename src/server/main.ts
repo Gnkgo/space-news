e88345e42eb17f5328548daf451c7280c2874793
Nick <ntroxler@ethz.ch>
2023-12-06 14:30:47 +0100

    clean up Nicks legacy backend

--- src/server/main.ts
+++ src/server/main.ts
@@ -178,34 +178,6 @@ regUrlApi<FireballReq, FireballRes>({
   genRes: async (res) => res as FireballRes
 });
 
-
-
-
-
-
-
-// VALENTIN END
-
-// NICK START
-/*app.get('/', (_req, res) => {
-  res.sendFile(path.resolve('src', 'client', 'html', 'home.html'));
-});
-app.get('/neo.html', (_req, res) => {
-  res.sendFile(path.resolve('src', 'client', 'html', 'neo.html'));
-});
-app.get('/nasa-cad-api', async (req, res) => {
-  try {
-    const {'date-min': dateMin, 'date-max': dateMax, 'dist-max': distMax} = req.query as {'date-min': string, 'date-max': string, 'dist-max': string};
-    const response = await fetch(`https://ssd-api.jpl.nasa.gov/cad.api?date-min=${dateMin}&date-max=%2B${dateMax}&dist-max=${distMax}`);
-    const data = await response.json();
-    res.json(data);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Internal Server Error');
-  }
-});*/
-// NICK END
-
 // Do not change below this line
 ViteExpress.listen(app, 5173, () =>
     console.log("Server is listening on http://localhost:5173"),
