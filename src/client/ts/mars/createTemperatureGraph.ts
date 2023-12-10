0487b99952417d86c52cff58b141ff63acad67c1
gnkgo <joanna.brodbeck@hotmail.ch>
2023-12-10 20:38:13 +0100

    Running docker

--- src/client/ts/mars/createTemperatureGraph.ts
+++ src/client/ts/mars/createTemperatureGraph.ts
@@ -48,6 +48,11 @@ export function extractAndDisplayTemperature(data: TemperatureData[], isCelcius:
         .append('g')
         .attr('transform', `translate(${margin.left},${margin.top})`);
 
+    const tooltip = d3.select('#mars-container').select('main').append('div')
+        .attr('class', 'tooltip')
+        .style('opacity', 0);
+
+
     const xScale = d3.scaleTime()
         .domain(d3.extent(dates) as [Date, Date])
         .range([0, containerWidth - margin.left - margin.right])
@@ -93,6 +98,8 @@ export function extractAndDisplayTemperature(data: TemperatureData[], isCelcius:
         .attr('height', height - margin.top - margin.bottom);
 
 
+    // ... (previous code)
+
     // Append path for average temperature line
     svg.append('path')
         .data([d3.range(dates.length)])
@@ -100,7 +107,39 @@ export function extractAndDisplayTemperature(data: TemperatureData[], isCelcius:
         .attr('d', avgLine)
         .attr('clip-path', 'url(#clip-path)')
         .style('stroke', 'white')
-        .style('fill', 'none');
+        .style('fill', 'none')
+        .on('mouseenter', function (event) {
+            handleMouseOver.call(this as SVGSVGElement, event);
+        })
+        .on('mouseleave', handleMouseOut);
+
+    // ... (remaining code)
+
+
+
+    function handleMouseOver(this: SVGSVGElement, event: d3.D3ZoomEvent<SVGSVGElement, unknown>) {
+        const bisectDate = d3.bisector((d: any) => d).left;
+        const [mouseX, mouseY] = d3.pointer(event, this);
+        const invertedX = xScale.invert(mouseX);
+        const index = bisectDate(dates, invertedX, 1);
+        const currentTemperature = averages[index];
+
+        tooltip.transition()
+            .duration(200)
+            .style('opacity', 0.9);
+
+        tooltip.html(`Temperature: ${currentTemperature} ${text}`)
+            .style('left', (mouseX + margin.left) + 'px') // Adjusted to consider margin.left
+            .style('top', (mouseY - 28 + margin.top) + 'px'); // Adjusted to consider margin.top
+    }
+
+    function handleMouseOut() {
+        tooltip.transition()
+            .duration(500)
+            .style('opacity', 0);
+    }
+
+
 
 
     svg.append('text')
